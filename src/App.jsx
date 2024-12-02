cee819bbcb4006068844b80f5418df9f1274a437
momunovN <152641534+momunovN@users.noreply.github.com>
2024-12-02 13:45:48 +0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import dots from './img/Dots.svg'
+
+function App() {
+  return (
+    <div className="b1">
+        <div className='flex justify-between py-3 px-4'>
+          <img className=''  src={dots} alt="" srcset="" />
+          <p className='font-bold'>SUMMER SALE UP TO 50% OFF</p>
+          <img className=''  src={dots} alt="" srcset="" />
+        </div>
+    </div>
+  );
+}
+
+export default App;
